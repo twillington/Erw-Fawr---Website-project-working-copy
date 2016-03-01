87fe656217f694b33990c08c7bac3ad22fe54dd4
twillington <tom.willington@hotmail.co.uk>
2016-03-01 15:30:17 +0000

    First Commit

---
+++ js/_vti_cnf/slider.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|TOM-PC\\Tom
+vti_modifiedby:SR|TOM-PC\\Tom
+vti_timelastmodified:TR|20 Jan 2014 18:51:07 -0000
+vti_timecreated:TR|20 Jan 2014 18:50:59 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_cacheddtm:TX|20 Jan 2014 18:51:08 -0000
+vti_filesize:IR|3
+vti_nexttolasttimemodified:TR|20 Jan 2014 18:50:59 -0000
